472e62c453e41d4866ab19915d1ab0d4369b6654
Thibaud REY <contact@thibaudrey.pro>
2017-11-25 12:56:13 +0100

    Add a logger and the post check action

--- API/src/AppBundle/Controller/DefaultController.php
+++ API/src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Monolog\Logger;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\BrowserKit\Response;
@@ -11,23 +12,38 @@ use Xeeeveee;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\View\View;
+use Service\Log;
 
 class DefaultController extends Controller
 {
 
     /**
-     * @Rest\Get("/size/{size}/solution/{solution}")
-     * @param $size
-     * @param $solution
-     * @return Response
+     * @Rest\Get("/size/{size}")
+     * @param int $size
+     * @return JsonResponse
+     * @internal param $solution
      */
-    public function getAction(int $size, bool $solution) {
+    public function getAction(int $size) {
+
+        $logger = new Log();
+
         $puzzle = new Xeeeveee\Sudoku\Puzzle();
         $puzzle->setCellSize($size);
         $puzzle->generatePuzzle();
         $puzzle->solve();
 
-        return new Response($puzzle);
+        $uuid = $logger->log($puzzle);
+
+        return new JsonResponse(['UUID' => $uuid, 'puzzle' => $puzzle]);
+    }
+
+
+    /**
+     * @Rest\("/check")
+     * @internal param array $try
+     */
+    public function postAction() {
+        var_dump($this->container->get('try'));
     }
 
 }
