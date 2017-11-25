1f3e106a024c1e4bd6929f8844ad018d4fce6f90
Aurélien Blais <aurelien.blais@whataniceplace.com>
2017-11-25 11:57:03 +0100

    Add basic countdown component, add it to helloworld

---
+++ src/components/Countdown.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="countdown">
+    <div class="segment" id="minutes">
+      {{ ("00" + countdown.minutes).slice(-2) }}
+    </div>
+    :
+    <div class="segment" id="seconds">
+      {{ ("00" + countdown.seconds).slice(-2) }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['countdown']
+}
+</script>
+
+<style scoped>
+  #countdown {
+    width: 200px;
+    height: 40px;
+    font-size: 22pt;
+    text-align: center;
+    letter-spacing: 8px;
+    border: 4px solid black;
+    border-radius: 10px;
+  }
+
+  #countdown * {
+    display: inline-block;
+  }
+
+  .segment {
+
+  }
+</style>
